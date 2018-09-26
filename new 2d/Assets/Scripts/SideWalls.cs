89b49d980df49d6bf746bebec63f468d3dbdbd1c
thewholehamdamily <furusethc@gmail.com>
2018-09-26 17:24:33 -0400

    simple pong

---
+++ new 2d/Assets/Scripts/SideWalls.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWalls : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "Ball")
+        {
+            string wallName = transform.name;
+            GameManager.Score(wallName);
+            collision.gameObject.SendMessage("RestartGame",1.0f, SendMessageOptions.RequireReceiver);
+        }
+    }
+}
